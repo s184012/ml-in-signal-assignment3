31f7bedf522b02526cd4dc3d330bd59b8da2edf7
frblanco <frblanco96@gmail.com>
2023-12-13 15:27:10 +0100

    Add necessary imports and loadmat function call

---
+++ problemset3.py
@@ -0,0 +1,19 @@
+import librosa
+import librosa.display
+import IPython
+import numpy as np
+from scipy import linalg
+from scipy import stats
+import scipy.signal
+from scipy.linalg import toeplitz
+from scipy.signal import wiener
+from scipy import signal
+import matplotlib.pyplot as plt
+import sounddevice as sd # only needed for playing
+import soundfile as sf # only needed if playing does not work
+from scipy.io import loadmat
+from IPython.display import Audio
+from crosscor_sol import crosscor
+
+problem= 'problem3_1'
+matdata = loadmat(f'{irname}.mat')
